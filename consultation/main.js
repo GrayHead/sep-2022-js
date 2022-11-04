7adf913eb79948d273ad1538a1056e2dc7811969
alex <AlexandrPetrov1987>
2022-11-04 20:58:44 +0200

    add consultation

---
+++ consultation/main.js
@@ -0,0 +1,11 @@
+let arr = [
+    {
+        name: 'vasay',
+        age: {
+            adsds: 22
+        }
+    }
+]
+console.log(arr[0].name);
+console.log(arr[0]['name']);
+console.log(arr[0]['age'].adsds);

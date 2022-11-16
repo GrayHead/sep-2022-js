79a21964133c6b83bfebfb5207cea8a72102cdf8
alex <AlexandrPetrov1987>
2022-11-16 20:49:50 +0200

    add resolve_hw5

--- resolves/resolve_hw5/main.js
+++ resolves/resolve_hw5/main.js
@@ -5,13 +5,13 @@
 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 
-// const squareCircle = (r) => Math.PI * Math.pow(r, 2);
+// let squareCircle = (r) => Math.PI * Math.pow(r, 2);
 // console.log(squareCircle(5));
 
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 
-// const cilinder = (r, h) => Math.PI * Math.pow(r, 2) * h;
-// console.log(cilinder(5, 2));
+// const cilindr = (r, h) => Math.PI * Math.pow(r, 2) * h;
+// console.log(cilindr(4, 2));
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
 
@@ -28,22 +28,18 @@
 // function foo(text) {
 //     document.write(`<p>${text}</p>`);
 // }
-// foo('hello');
+// foo('Hello');
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
 // function foo(text) {
-//     document.write(`
-//     <ul>
-//          <li>${text}</li>
-//          <li>${text}</li>
-//          <li>${text}</li>
-//          <li>${text}</li>
-//     </ul>
-//     `)
+//     document.write(`<ul>
+//                        <li>${text}</li>
+//                        <li>${text}</li>
+//                        <li>${text}</li>
+//                     </ul>`)
 // }
-//
-// foo('hello world');
+// foo('hello');
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість
 // li визначається другим аргументом, який є числовим (тут використовувати цикл)
@@ -51,25 +47,23 @@
 // function foo(text,num) {
 //     document.write(`<ul>`)
 //     for (let i = 0; i < num; i++) {
-//         document.write(`<li>${text}</li>`)
+//         document.write(`<li>${text}</li>`);
 //     }
-//
 //     document.write(`</ul>`)
 // }
-// foo('hello',10);
+// foo('hello',5);
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
-// let arr = [1, 2, true, false, 'str'];
-//
-// const elem = function (array) {
+// let arr = [12,23,true,false,'str'];
+// function foo(array) {
 //     document.write(`<ul>`)
 //     for (const item of array) {
 //         document.write(`<li>${item}</li>`);
 //     }
 //     document.write(`</ul>`)
 // }
-// elem(arr);
+// foo(arr);
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для ' +
 // 'кожного об'єкту окремий блок.
@@ -85,18 +79,17 @@
 //     }
 // ]
 //
-// const users = function (array) {
-//     for (const user of array) {
-//         document.write(`<div>${user.name} -- ${user.age}</div>`)
+// let objArr = function (array) {
+//     for (const item of array) {
+//         document.write(`<div>${item.name} -- ${item.age}</div>`)
 //     }
 // }
-// users(arr);
+// objArr(arr);
 
 // - створити функцію яка повертає найменьше число з масиву
 
-// let arr = [1, 2, 3, 4, 55, 66, -1];
-//
-// function foo(array) {
+// const arr = [1,2,-4,0,100500];
+// const minItem = function (array) {
 //     let min = array[0];
 //     for (const item of array) {
 //         if (item < min) {
@@ -105,21 +98,32 @@
 //     }
 //     return min;
 // }
-// let foo1 = foo(arr);
-// console.log(foo1);
+// let minItem1 = minItem(arr);
+// console.log(minItem1);
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
-let arr = [1,2,3];
-function foo(array) {
-    let sum = 0;
-    for (const item of array) {
-        sum += item;  // sum = sum + item
-    }
-    return sum;
-}
+// const arr = [1,2,3];
+// const foo = (array) => {
+//     let sum = 0;
+//     for (const item of array) {
+//         sum += item;
+//     }
+//     return sum;
+// };
+// let number = foo(arr);
+// console.log(number);
 
-console.log(foo(arr));
+let i = 0;
+let n = 0;
+while (i < 5) {
+    i++;
+    if (i === 3) {
+        continue;
+    }
+    n += i; // 1 , 3, 7, 12
+    console.log(n);
+}
 
 
 

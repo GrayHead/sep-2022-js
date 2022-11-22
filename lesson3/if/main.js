b9c51bda15214fca9b82c348445063dabccadc25
GrayHead <ss.zhuravlov@gmail.com>
2022-11-22 15:02:55 +0200

    lesson 2

---
+++ lesson3/if/main.js
@@ -0,0 +1,92 @@
+// let ansrew = confirm('ти страший за 17?');
+//
+//
+// if (ansrew) {
+//     document.write('<h1>CORN</h1>');
+// } else {
+//     document.write('<h1>cartoon</h1>');
+// }
+
+
+// let age = +prompt('how old are you?');
+//
+// if (age < 18) {
+//     document.write('<h1>cartoon</h1>');
+// } else if (age >= 18 && age < 90) {
+//     document.write('<h1>corn hub</h1>');
+// } else if (age >= 90) {
+//     document.write('<h1>casablanca</h1>');
+// } else {
+//     document.write('<h1>????</h1>');
+// }
+
+
+// let color = prompt('enter color that you see');
+// let roadStatus = confirm('road is clear');
+//
+// // OR
+// if (color === 'green' || roadStatus) {
+//     console.log('you can go');
+// } else if (color === 'yellow') {
+//     console.log('wait');
+// } else if (color === 'red') {
+//     console.log('stop');
+// }
+
+// let x;
+//
+// if (true) {
+//     x = 100;
+// } else {
+//     x = -100;
+// }
+//
+// console.log(x);
+
+// let y = confirm('what?') ? 100 : -100;
+// console.log(y);
+
+
+// console.log(!!'');
+// console.log(!!0);
+// console.log(!!NaN);
+// console.log(!!undefined);
+// console.log(!!null);
+//
+// if ('') {
+//     console.log('good');
+// } else {
+//     console.log('bad');
+// }
+
+
+// let arr = [];
+//
+// if (arr.length) {
+//     console.log('array not empty');
+// } else {
+//     console.log(' empty');
+// }
+
+
+// if () {
+// }else {}
+
+// switch ('green') {
+//     case 'green':
+//     case 'yellow':
+//         console.log('go');
+//         break;
+//     case 'red':
+//         console.log('stop');
+//         break;
+//     default:
+//         console.log('?????');
+// }
+
+
+// let xxx = prompt() || 'asdgfahgdsf' ;
+// console.log(xxx);
+
+let x = 10;
+console.log(++x);

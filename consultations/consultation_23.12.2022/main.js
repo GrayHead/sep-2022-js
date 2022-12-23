54082bde056ca70e31b7aa04e77dc785a9b08a6a
alex <AlexandrPetrov1987>
2022-12-23 20:26:07 +0200

    add consultation_23.12.2022

---
+++ consultations/consultation_23.12.2022/main.js
@@ -0,0 +1,17 @@
+// Отримати коментарі
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+//     .then(value => value.json())
+//     .then(value => {});
+
+
+// Натискання на кнопку
+// btn.onclick() = () => {
+//
+//     const anchorElement = document.createElement('a');
+//     anchorElement.href = `example/details.html?id=${user.id}`;
+//     anchorElement.innerText = `Click`;
+//
+//     btn.append(anchorElement);
+//
+//     // location.href = `details.html?id=${user.id}`;
+// }
